afd49c97daff7a69dcb4987f6f8bd5964d1b94c7
mahi97 <mohammadmahdi76@gmail.com>
2017-06-11 00:29:05 +0430

    fix main node

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 var http = require('http');
 var url  = require('url');
 var fs   = require('fs');
-// http://127.0.0.1:8080/save?username=imah&password=adsfasdf&firstname=madsfuh&lastname=sdajkfh&email=mohammad%40mahdi.com&BD=asdjfkh&tel=dsajkfh&homepage=&age=12&job=adsfjk
+const db = require('./connect');
+
+db.creatTable();
 http.createServer(function (req, res) {
   var u = url.parse(req.url, true);
   console.log();
   if (u.pathname == '/save') {
     var q = u.query;
+
+    var result = false;
     console.log(q);
-    res.end();
+    db.addUser(
+      q.firstname,
+      q.lastname,
+      q.username,
+      q.password,
+      q.email,
+      q.tel,
+      q.homepage,
+      q.age,
+      q.job,
+      function(e) {
+        console.log("USER FAILD");
+      });
+      fs.readFile("./save.html", function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+
   } else {
 
     fs.readFile("./index.html", function(err, data) {
